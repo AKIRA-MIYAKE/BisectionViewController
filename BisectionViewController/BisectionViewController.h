21a98da91cb5896c4305d7f6faff385e75d521c6
AKIRA-MIAYEK <akira.aktk.miyake@gmail.com>
2015-06-07 15:40:12 +0900

    Initial Commit

---
+++ BisectionViewController/BisectionViewController.h
@@ -0,0 +1,19 @@
+//
+//  BisectionViewController.h
+//  BisectionViewController
+//
+//  Created by MiyakeAkira on 2015/06/07.
+//  Copyright (c) 2015年 Miyake Akira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BisectionViewController.
+FOUNDATION_EXPORT double BisectionViewControllerVersionNumber;
+
+//! Project version string for BisectionViewController.
+FOUNDATION_EXPORT const unsigned char BisectionViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BisectionViewController/PublicHeader.h>
+
+
